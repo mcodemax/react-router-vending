26fcbfb8d37da5adef55fe417b5c0ddf24792c42
Maxwell Johnson <63630792+mcodemax@users.noreply.github.com>
2021-11-16 08:54:59 -0800

    Update Snack.js

--- src/Snack.js
+++ src/Snack.js
@@ -5,7 +5,7 @@ function Snack({imgUrl, altText, imageName}) {
   return (
     <>
       {/*return a snack pic*/}
-      <img src={imgUrl} alt={altText} width="500" height="600">{imageName}</img>
+      <img src={imgUrl} alt={altText} width="500" height="600"></img>
     </>
   );
 }
