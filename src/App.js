4d424a95c055960635a161a93ac3428a5659d47d
mcodemax <mwalterjohnson7@gmail.com>
2021-11-16 18:56:09 -0800

    will be overwritten later

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from 'react';
 import './App.css';
+import VendingMachine from './VendingMachine';
 
 function App() {
   return (
     <div className="App">
-      
+      <VendingMachine />
     </div>
   );
 }
---
+++ src/VendingMachine.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Snack from "./Snack";
+
+const VendingMachine = () => {
+    
+    return (
+        <>
+        </>
+    )
+}
+
+export default VendingMachine;
